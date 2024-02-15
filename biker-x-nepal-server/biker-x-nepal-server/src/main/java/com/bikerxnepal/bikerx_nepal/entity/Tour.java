febb0a767b51d0c9f866a7965b9aaa2eaa282d17
Ashish Mool <asis.mool@gmail.com>
2024-02-15 13:59:16 +0545

    Tour Entity Update
    -- Tour Entity

---
+++ biker-x-nepal-server/biker-x-nepal-server/src/main/java/com/bikerxnepal/bikerx_nepal/entity/Tour.java
@@ -0,0 +1,53 @@
+package com.bikerxnepal.bikerx_nepal.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tours")
+@Getter
+@Setter
+public class Tour {
+
+    @Id
+    @SequenceGenerator(name = "tours_seq_gen", sequenceName = "tours_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "tours_seq_gen", strategy = GenerationType.SEQUENCE)
+    private Long tourId;
+
+    @Column(name = "tour_name", nullable = false)
+    private String tourName;
+
+    @Column(name = "description", nullable = false)
+    private String tourDescription;
+
+    @Column(name = "type", nullable = false)
+    private String tourType;
+
+    @Column(name = "itinerary", nullable = true)
+    private String tourItinerary;
+
+    @Column(name = "start_date", nullable = false)
+    private Date startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private Date endDate;
+
+    @Column(name = "max_participants", nullable = false)
+    private Integer maxParticipants;
+
+    @Column(name = "rating", nullable = true)
+    private Long tourRating;
+
+    @Column(name = "tour_price", nullable = false)
+    private Double tourPrice;
+
+    @Column(name = "availability", nullable = false)
+    private Boolean tourAvailability;
+
+    private String image;
+
+
+}
